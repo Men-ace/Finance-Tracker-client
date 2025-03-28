77f1c285268de46015c804049d051fcb11827599
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-03-28 21:31:57 +1100

    made login and signup optins with jwt authentications

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,25 @@
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FinincalTips from "../components/FinincalTips";
+import SignUpForm from "../components/SignUpForm";
+
+const Signup = () => {
+  return (
+    <Container className="p-3">
+      <Row
+        className=" p-5  rounded"
+      >
+        <Col md={6} style={{ backgroundColor: "black" }}>
+          <FinincalTips />
+        </Col>
+        <Col md={6} style={{ backgroundColor: "black" }}>
+          <SignUpForm />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Signup;
